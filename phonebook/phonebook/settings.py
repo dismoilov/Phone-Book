89f8b07b4799be30826e54cf0f2010821fc33981
Windows <ryadovoyzadrot007@gmail.com>
2024-02-25 18:55:16 +0500

    main

--- phonebook/phonebook/settings.py
+++ phonebook/phonebook/settings.py
@@ -115,21 +115,10 @@ WSGI_APPLICATION = 'phonebook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'PhoneBook',
-        'USER': 'PhoneBook',
-        'PASSWORD': '!Qazxsw2',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
